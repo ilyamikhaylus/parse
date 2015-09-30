69975c1f2d86a2c2b3c49341cdfa552e94610d58
Ilya Mikhaylus <ilyamikhaylus@gmail.com>
2015-09-30 22:18:23 +0300

    Update parse_hotline.js

--- parse_hotline.js
+++ parse_hotline.js
@@ -1,23 +1,20 @@
-
-	var fs = require('fs');
-
-	var path = 'output.txt';
+var fs = require('fs');
+var path = 'output.txt';
 
 var webpage = require('webpage').create();
 webpage
   .open('http://hotline.ua/mobile-mobilnye-telefony-i-smartfony/apple-iphone-5s-32gb-gold/') 
   .then(function(){ 
-	var someContent = webpage.evaluate(function () { // после того как динамический контент подгружен, можно его спарсить
+	var someContent = webpage.evaluate(function () {
 		return document.querySelector('a.range-price > strong').textContent.toString();
     });
-	var s = someContent.replace(/\D/g,'');
-	var b = someContent.split(" – ");
-	var c1 = parseInt(b[0].replace(/\D/g,''));
-	var c2 = parseInt(b[1].replace(/\D/g,''));
-	var c = ((c2+c1)/2).toFixed(); 
-	console.log(c);
+	var range = someContent.split(" – ");
+	var range[0] = parseInt(range[0].replace(/\D/g,''));
+	var range[1] = parseInt(range[1].replace(/\D/g,''));
+	var avrgPrice = ((range[0]+range[1])/2).toFixed(); 
+	console.log(avrgPrice);
 	
-	fs.write(path, c, 'w');
+	fs.write(path, avrgPrice, 'w');
 
 	phantom.exit();
 	slimer.exit();
